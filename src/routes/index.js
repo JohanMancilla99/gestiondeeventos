27439aa834c6632ae2cc20cb3326b0c83cf55f17
Johan <johanmancilla99@gmail.com>
2020-06-01 16:18:57 -0500

    se creo en un 80% la ventana de inicio

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {
+        cssFiles: [
+            "/css/inicio-header.css", 
+            "/css/index.css"
+        ]
+    });
 });
 
 
